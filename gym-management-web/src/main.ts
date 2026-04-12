ac13285695cbfc2844affcf22f7b22ec79b55be0
Manoj Narasareddy <mn9353780784@gmail.com>
2026-04-12 22:25:29 +0530

    added speed insights

--- gym-management-web/src/main.ts
+++ gym-management-web/src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
+
 
 bootstrapApplication(AppComponent, appConfig)
+  .then(() => {
+    inject();
+    injectSpeedInsights();
+  })
   .catch((err) => console.error(err));
